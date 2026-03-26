e3cf7d0e2fa343ef6e557fd31ac4ed065bde10a3
yann97one <yannissileber@gmail.com>
2026-03-26 10:24:52 +0100

    adding of client url as allowed origin for cors

--- server/main.py
+++ server/main.py
@@ -1,9 +1,12 @@
+import os
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import SQLModel
 from database import engine
 from router import students, teachers, marks
+
+origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:5175").split(",")
 
 
 @asynccontextmanager
@@ -16,7 +19,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5175"],
+    allow_origins=origins,
     allow_methods=["*"],
     allow_headers=["*"],
 )
